6b2b62eb5aa1ed7c4a204e3ea7d924dec4999e71
Avimita-amc8313 <77707738+Avimita-amc8313@users.noreply.github.com>
2025-05-05 12:53:56 -0400

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -153,13 +153,11 @@ $(document).ready(function () {
   });
 
      // Automatically open all external links in a new tab
-   $('a[href]').each(function () {
-  const href = $(this).attr('href');
-  const isExternal = href.startsWith('http') && !href.includes(location.hostname);
-
-  if (isExternal) {
-    $(this).attr('target', '_blank').attr('rel', 'noopener noreferrer');
-  }
-});
+      $('a[href^="http"]').each(function () {
+     if (!this.href.includes(location.hostname)) {
+       console.log("Updating external link:", this.href);  // ← log the link
+       $(this).attr('target', '_blank').attr('rel', 'noopener noreferrer');
+     }
+   });
    
 });
